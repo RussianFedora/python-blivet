12cf58120e4acf292bb2158a5fa5f7099ecdccb6
Tigro <ashejn@gmail.com>
2013-11-10 22:14:29 +0400

    update to 0.23.4

--- get_sources.sh
+++ get_sources.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -sO http://pkgs.fedoraproject.org/repo/pkgs/python-blivet/blivet-0.23.2.tar.gz/b2e7ba1cb7372071db24070358874219/blivet-0.23.2.tar.gz
+curl -sO http://pkgs.fedoraproject.org/repo/pkgs/python-blivet/blivet-0.23.4.tar.gz/b94874b20e27c7e775598e075702f1ee/blivet-0.23.4.tar.gz
 
