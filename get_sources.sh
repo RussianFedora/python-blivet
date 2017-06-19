b1f251c999fde76a4aa7ab01b6fb67a3f6ab1bce
Tigro <ashejn@gmail.com>
2017-06-19 12:26:24 +0300

    added get source file

---
+++ get_sources.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -sO http://pkgs.fedoraproject.org/repo/pkgs/python-blivet/blivet-2.1.9.tar.gz/sha512/18765fdcb27ba851bc3f3640a880891f43a15ca93225eef086aa240da173686eeb06304f6abd4d736cb3e6b76498cca3585a1be7d03a4c7de4dd88372cf869b1/blivet-2.1.9.tar.gz
